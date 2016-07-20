0e2ff9909bf5d0e50827f11ffb802b823ffb77ad
bharat <bharat@princes-macbook-pro.local>
2016-07-20 10:04:39 +0530

    Initial Commit

---
+++ BoxBazar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BoxBazar
+//
+//  Created by bharat on 20/07/16.
+//  Copyright © 2016 Bharat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BoxBazar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BoxBazar
+//
+//  Created by bharat on 20/07/16.
+//  Copyright © 2016 Bharat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
